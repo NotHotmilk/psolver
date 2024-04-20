e5431a291a882ee3135662237681a64832b86778
NotHotmilk <The_Hotmilk@icloud.com>
2024-04-20 16:04:31 +0900

    App

---
+++ solvers/practice/Test/Test5.py
@@ -0,0 +1,62 @@
+﻿import sys
+import subprocess
+
+import cspuz
+from cspuz import Solver, graph
+from cspuz.constraints import count_true, fold_and, fold_or, then
+from cspuz.puzzle.util import stringify_grid_frame, stringify_array, encode_array
+from cspuz.generator import generate_problem, count_non_default_values, ArrayBuilder2D
+import common_rules
+
+
+def solve_original(height, width, problem):
+    solver = Solver()
+    is_black = solver.bool_array((height, width))
+    solver.add_answer_key(is_black)
+
+    common_rules.all_black_blocks_have_same_area_2(solver, is_black, problem, height, width)
+    common_rules.minesweeper_like_around_number(solver, is_black, problem, height, width)
+    common_rules.not_forming_2by2_square(solver, ~is_black)
+
+    has_answer = solver.solve()
+    return has_answer, is_black
+
+
+def generate_original(height, width, symmetry=False, verbose=False):
+    generated = generate_problem(lambda problem: solve_original(height, width, problem),
+                                 builder_pattern=ArrayBuilder2D(height, width, [-1, 0, 1, 2, 3, 4, 5], default=-1,
+                                                                symmetry=symmetry),
+                                 clue_penalty=lambda problem: count_non_default_values(problem, default=-1, weight=10),
+                                 verbose=verbose)
+    return generated
+
+
+
+def generatehxw(height, width):
+    print("Generating")
+    problem = generate_original(height, width, symmetry=False, verbose=True)
+    print(stringify_array(problem, str))
+    link = 'https://puzz.link/p?nurikabe/{}/{}/{}'.format(width, height, encode_array(
+        list(map(lambda row: list(map(lambda x: '_' if x == -1 else x, row)), problem))
+    ))
+    print(link)
+    print(stringify_array(solve_original(height, width, problem)[1], {True: '#', False: '.', None: '?'}))
+    return link
+
+import multiprocessing
+
+def parallel_generatehxw():
+    with multiprocessing.Pool(processes=4) as pool:
+        results = pool.map(generatehxw_wrapper, range(4))
+    return results
+
+
+def generatehxw_wrapper(_):
+    return generatehxw(4, 4)
+
+
+if __name__ == "__main__":
+    results = parallel_generatehxw()
+    print("Result")
+    for result in results:
+        print(result)
---
+++ solvers/puzzles/SdouriSolver.py
@@ -0,0 +1,81 @@
+﻿import sys
+import subprocess
+
+import cspuz
+from cspuz import Solver, graph
+from cspuz.constraints import count_true, fold_and, fold_or, then
+from cspuz.puzzle.util import stringify_grid_frame, stringify_array, encode_array
+from cspuz.generator import generate_problem, count_non_default_values, ArrayBuilder2D
+
+
+def solve_sdouri(height, width, problem):
+    solver = Solver()
+    grid_frame = graph.BoolGridFrame(solver, height - 1, width - 1)
+    solver.add_answer_key(grid_frame)
+
+    is_passed = graph.active_edges_single_cycle(solver, grid_frame)
+
+    # 点y,xで線がまっすぐに伸びるbool
+    def is_straight(y, x):
+        v = grid_frame.vertical[y - 1:y + 1, x]
+        h = grid_frame.horizontal[y, x - 1:x + 1]
+        return (count_true(v) == 2) ^ (count_true(h) == 2)
+
+    for y in range(height):
+        for x in range(width):
+            if problem[y][x] != -1:
+                solver.ensure(~is_passed[y, x])
+                cells = [(y + dy, x + dx)
+                         for dy in range(-1, 2)
+                         for dx in range(-1, 2)
+                         if 0 <= y + dy < height and 0 <= x + dx < width]
+                solver.ensure(count_true([is_straight(y, x) for y, x in cells]) == problem[y][x])
+
+    has_answer = solver.solve()
+    return has_answer, grid_frame
+
+def generate_sdouri(height, width, symmetry=False, verbose=False):
+
+    def penalty(problem):
+        ret = 0
+        for y in range(height):
+            for x in range(width):
+                if problem[y][x] == 0:
+                    ret += 16
+                elif problem[y][x] == 6:
+                    ret += 14
+                elif problem[y][x] != -1:
+                    ret += 10
+        return ret
+    
+    generated = generate_problem(lambda problem: solve_sdouri(height, width, problem),
+                                    builder_pattern=ArrayBuilder2D(height, width, [i for i in range(-1, 7)], default=-1, symmetry=symmetry),
+                                    clue_penalty=penalty,#lambda problem: count_non_default_values(problem, default=-1, weight=10),
+                                    verbose=verbose)
+    return generated
+
+def generatehxw(height, width):
+    print("Generating")
+    problem = generate_sdouri(height, width, symmetry=False, verbose=True)
+    print(stringify_array(problem, str))
+    link = 'https://puzz.link/p?tapaloop/{}/{}/{}'.format(width, height, encode_array(
+        list(map(lambda row: list(map(lambda x: '_' if x == -1 else x, row)), problem))
+    ))
+    print(link)
+    print(stringify_grid_frame(solve_sdouri(height, width, problem)[1]))
+    return link
+
+import multiprocessing
+
+def parallel_generatehxw():
+    with multiprocessing.Pool(processes=4) as pool:
+        results = pool.map(generatehxw_wrapper, range(4))
+    return results
+
+def generatehxw_wrapper(i):
+    return generatehxw(8, 8)
+
+if __name__ == '__main__':
+    results = parallel_generatehxw()
+    for result in results:
+        print(result)
